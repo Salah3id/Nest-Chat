bb6a2f4282e41bbfcbeabd440fc89644659075fb
unknown <salah.enm@gmail.com>
2022-01-17 12:57:24 +0200

    implement WebSocketGateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { BlogModule } from './blog/blog.module';
+// import { AppGateway } from './app.gateway';
+
+// @Module({
+//   imports: [BlogModule],
+//   controllers: [AppController],
+//   providers: [AppService, AppGateway],
+// })
+// export class AppModule {}
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { BlogModule } from './blog/blog.module';
 import { AppGateway } from './app.gateway';
 
 @Module({
-  imports: [BlogModule],
-  controllers: [AppController],
-  providers: [AppService, AppGateway],
+ imports: [],
+ controllers: [],
+ providers: [AppGateway],
 })
 export class AppModule {}
